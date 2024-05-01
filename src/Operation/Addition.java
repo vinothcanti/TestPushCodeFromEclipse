4784dfb16e7cdbb5a77ad9ac4947562f26a3512d
vinothraj <vinothcanti@gmail.com>
2024-05-01 15:14:03 +0530

    "Added code"

---
+++ src/Operation/Addition.java
@@ -0,0 +1,13 @@
+package Operation;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 10;
+		int sum = x +y;
+		System.out.println(sum);
+	}
+
+}
